0a50b1b8218f50d64dd549da6ffa140eb087e1b4
midia-hamdi <midiahamdi367@gmail.com>
2023-07-03 14:43:12 +0330

    add conditional

---
+++ src/Conditional.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Conditional extends Component {
+  render() {
+    return (
+      <div>Conditional</div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Conditional from "./Conditional"
+
+ReactDOM.render(<Conditional />, document.getElementById('root'))
+
+
